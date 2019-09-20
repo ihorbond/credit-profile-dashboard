b7e3bb67122c6baa2bf3ebb9a5bad2f4b0dbadb2
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-09-20 16:28:10 -0400

    Add project files.

---
+++ PortalDB/SchemaModifications/dbo.CreditProfile.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[CreditProfile]
+(
+	[Id] INT NOT NULL , 
+    [LineOfCredit] MONEY NOT NULL, 
+    [Balance] MONEY NOT NULL DEFAULT 0, 
+    [CustomerId] INT NOT NULL, 
+    [CreatedBy] INT NOT NULL, 
+    [CreatedOn] DATETIME2(1) NULL DEFAULT GETDATE(), 
+    [ModifiedBy] INT NULL, 
+    [ModifiedOn] DATETIME2(1) NULL, 
+    CONSTRAINT [PK_CreditProfile] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_CreditProfile_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [Customer]([Id])
+)
---
+++ PortalDB/SchemaModifications/dbo.Customer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Id] INT NOT NULL , 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [CreatedBy] INT NOT NULL, 
+    [CreatedOn] DATETIME2(1) NULL DEFAULT GETDATE(), 
+    [ModifiedBy] INT NULL, 
+    [ModifiedOn] DATETIME2(1) NULL, 
+    CONSTRAINT [PK_Customer] PRIMARY KEY ([Id])
+)
